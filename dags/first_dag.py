62b27be15bd055a61ee65feef9a0b7da297b3d62
jefsuu <jdjbubu28@gmail.com>
2023-05-22 22:00:39 -0300

    first dag

--- dags/first_dag.py
+++ dags/first_dag.py
@@ -27,18 +27,43 @@ default_args = {
 with DAG(
     "tutorial",
     default_args=default_args,
-    schedule=None
+    schedule=None,
+    start_date=datetime(2023,5,22)
 ) as dag:
     t1 = BashOperator(
-        'print_date',
+        task_id='print_date',
         bash_command='date'
     )
 
+    t1.doc_md = dedent(
+    """\
+#### Task Documentation
+You can document your task using the attributes `doc_md` (markdown),
+`doc` (plain text), `doc_rst`, `doc_json`, `doc_yaml` which gets
+rendered in the UI's Task Instance Details page.
+![img](http://montcs.bloomu.edu/~bobmon/Semesters/2012-01/491/import%20soul.png)
+**Image Credit:** Randall Munroe, [XKCD](https://xkcd.com/license.html)
+"""
+)
+
     t2 = BashOperator(
-        'sleep',
+        task_id='sleep',
         depends_on_past=False,
         bash_command='sleep 5',
         retries=3
     )
 
-    t1 >> t2
+    templated_command = dedent(
+    """
+{% for i in range(5) %}
+    echo "{{ ds }}"
+    echo "{{ macros.ds_add(ds, 7)}}"
+{% endfor %}
+"""
+)
+    t3 = BashOperator(
+    task_id="templated",
+    depends_on_past=False,
+    bash_command=templated_command,
+)
+    t1 >> [t2, t3]
